fc3ec5feda2050db63b300a7d928a6af318c1a7c
Rudraksh-2025 <rud.crestcoder@gmail.com>
2025-01-31 16:37:47 +0530

    finallllll

--- frontend/dist/assets/latency-chart-Bj5OSYzg.es-B3guBAtD.js
+++ frontend/dist/assets/latency-chart-Bj5OSYzg.es-B3guBAtD.js
@@ -1,4 +1,4 @@
-var Ui=Object.defineProperty;var Yi=(s,t,e)=>t in s?Ui(s,t,{enumerable:!0,configurable:!0,writable:!0,value:e}):s[t]=e;var k=(s,t,e)=>Yi(s,typeof t!="symbol"?t+"":t,e);import{r as $,R as hi,j as es}from"./index-B53lX2uJ.js";/*!
+var Ui=Object.defineProperty;var Yi=(s,t,e)=>t in s?Ui(s,t,{enumerable:!0,configurable:!0,writable:!0,value:e}):s[t]=e;var k=(s,t,e)=>Yi(s,typeof t!="symbol"?t+"":t,e);import{r as $,R as hi,j as es}from"./index-BQFoqWKS.js";/*!
  * @kurkle/color v0.3.4
  * https://github.com/kurkle/color#readme
  * (c) 2024 Jukka Kurkela
--- frontend/src/store/useSocketStore.jsx
+++ frontend/src/store/useSocketStore.jsx
@@ -1,149 +1,149 @@
-import { create } from 'zustand';
-import { socket } from '../utils/Socket.js';
+// import { create } from 'zustand';
+// import { socket } from '../utils/Socket.js';
 
-export const useSocketStore = create((set, get) => {
-    let peerConnection = null; // Regular variable for peer connection
+// export const useSocketStore = create((set, get) => {
+//     let peerConnection = null; // Regular variable for peer connection
 
-    const iceServers = {
-        iceServers: [{ urls: 'stun:stun.l.google.com:19302' }],
-    };
+//     const iceServers = {
+//         iceServers: [{ urls: 'stun:stun.l.google.com:19302' }],
+//     };
 
-    return {
-        isCalling: false,
-        peerConnection: null,
-        isReceiving: false,
-        localStream: null,
-        remoteStream: null,
-        callData: null,
+//     return {
+//         isCalling: false,
+//         peerConnection: null,
+//         isReceiving: false,
+//         localStream: null,
+//         remoteStream: null,
+//         callData: null,
 
-        // Initialize local media
-        initLocalStream: async () => {
-            try {
-                const stream = await navigator.mediaDevices.getUserMedia({
-                    video: true,
-                    audio: true,
-                });
-                set({ localStream: stream });
-            } catch (error) {
-                console.error('Error accessing media devices:', error);
-            }
-        },
+//         // Initialize local media
+//         initLocalStream: async () => {
+//             try {
+//                 const stream = await navigator.mediaDevices.getUserMedia({
+//                     video: true,
+//                     audio: true,
+//                 });
+//                 set({ localStream: stream });
+//             } catch (error) {
+//                 console.error('Error accessing media devices:', error);
+//             }
+//         },
 
-        startCall: async (to) => {
-            set({ isCalling: true });
+//         startCall: async (to) => {
+//             set({ isCalling: true });
 
-            const { localStream, initLocalStream } = get();
+//             const { localStream, initLocalStream } = get();
 
-            if (!localStream) {
-                await initLocalStream();
-            }
+//             if (!localStream) {
+//                 await initLocalStream();
+//             }
 
-            const currentStream = get().localStream;
-            if (!currentStream) {
-                console.error('Failed to initialize local media stream.');
-                set({ isCalling: false });
-                return;
-            }
+//             const currentStream = get().localStream;
+//             if (!currentStream) {
+//                 console.error('Failed to initialize local media stream.');
+//                 set({ isCalling: false });
+//                 return;
+//             }
 
-            peerConnection = new RTCPeerConnection(iceServers);
-            set({ peerConnection });
-            currentStream.getTracks().forEach((track) =>
-                peerConnection.addTrack(track, currentStream)
-            );
+//             peerConnection = new RTCPeerConnection(iceServers);
+//             set({ peerConnection });
+//             currentStream.getTracks().forEach((track) =>
+//                 peerConnection.addTrack(track, currentStream)
+//             );
 
-            peerConnection.onicecandidate = (event) => {
-                if (event.candidate) {
-                    socket.emit('send-candidate', { to, candidate: event.candidate });
-                }
-            };
+//             peerConnection.onicecandidate = (event) => {
+//                 if (event.candidate) {
+//                     socket.emit('send-candidate', { to, candidate: event.candidate });
+//                 }
+//             };
 
-            peerConnection.ontrack = (event) => {
-                set({ remoteStream: event.streams[0] });
-            };
+//             peerConnection.ontrack = (event) => {
+//                 set({ remoteStream: event.streams[0] });
+//             };
 
-            try {
-                const offer = await peerConnection.createOffer();
-                await peerConnection.setLocalDescription(offer);
-                console.log('Sending offer to:', to);
-                socket.emit('call-user', { to, offer });
-            } catch (error) {
-                console.error('Error starting call:', error);
-                set({ isCalling: false });
-            }
-        },
+//             try {
+//                 const offer = await peerConnection.createOffer();
+//                 await peerConnection.setLocalDescription(offer);
+//                 console.log('Sending offer to:', to);
+//                 socket.emit('call-user', { to, offer });
+//             } catch (error) {
+//                 console.error('Error starting call:', error);
+//                 set({ isCalling: false });
+//             }
+//         },
 
-        answerCall: async () => {
-            const { callData, localStream, initLocalStream } = get();
-            if (!localStream) await initLocalStream();
+//         answerCall: async () => {
+//             const { callData, localStream, initLocalStream } = get();
+//             if (!localStream) await initLocalStream();
 
-            peerConnection = new RTCPeerConnection(iceServers);
-            localStream.getTracks().forEach((track) => peerConnection.addTrack(track, localStream));
+//             peerConnection = new RTCPeerConnection(iceServers);
+//             localStream.getTracks().forEach((track) => peerConnection.addTrack(track, localStream));
 
-            peerConnection.onicecandidate = (event) => {
-                if (event.candidate) {
-                    socket.emit('send-candidate', { to: callData.from, candidate: event.candidate });
-                }
-            };
+//             peerConnection.onicecandidate = (event) => {
+//                 if (event.candidate) {
+//                     socket.emit('send-candidate', { to: callData.from, candidate: event.candidate });
+//                 }
+//             };
 
-            peerConnection.ontrack = (event) => {
-                set({ remoteStream: event.streams[0] });
-            };
+//             peerConnection.ontrack = (event) => {
+//                 set({ remoteStream: event.streams[0] });
+//             };
 
-            try {
-                await peerConnection.setRemoteDescription(new RTCSessionDescription(callData.offer));
-                const answer = await peerConnection.createAnswer();
-                await peerConnection.setLocalDescription(answer);
-                socket.emit('answer-call', { to: callData.from, answer });
-            } catch (error) {
-                console.error('Error answering call:', error);
-            }
+//             try {
+//                 await peerConnection.setRemoteDescription(new RTCSessionDescription(callData.offer));
+//                 const answer = await peerConnection.createAnswer();
+//                 await peerConnection.setLocalDescription(answer);
+//                 socket.emit('answer-call', { to: callData.from, answer });
+//             } catch (error) {
+//                 console.error('Error answering call:', error);
+//             }
 
-            set({ isReceiving: false });
-        },
+//             set({ isReceiving: false });
+//         },
 
-        endCall: (to) => {
-            let { peerConnection } = get();
-            if (peerConnection) {
-                peerConnection.close();
-                peerConnection = null;
-            }
+//         endCall: (to) => {
+//             let { peerConnection } = get();
+//             if (peerConnection) {
+//                 peerConnection.close();
+//                 peerConnection = null;
+//             }
 
-            const { localStream } = get();
-            if (localStream) {
-                localStream.getTracks().forEach((track) => track.stop());
-                set({ localStream: null });
-            }
+//             const { localStream } = get();
+//             if (localStream) {
+//                 localStream.getTracks().forEach((track) => track.stop());
+//                 set({ localStream: null });
+//             }
 
-            set({ isCalling: false, isReceiving: false, remoteStream: null });
-            socket.emit('end-call', { to });
-        },
+//             set({ isCalling: false, isReceiving: false, remoteStream: null });
+//             socket.emit('end-call', { to });
+//         },
 
-        addCandidate: (candidate) => {
-            const { peerConnection } = get();
-            if (peerConnection) {
-                peerConnection.addIceCandidate(new RTCIceCandidate(candidate));
-            }
-        },
+//         addCandidate: (candidate) => {
+//             const { peerConnection } = get();
+//             if (peerConnection) {
+//                 peerConnection.addIceCandidate(new RTCIceCandidate(candidate));
+//             }
+//         },
 
-        handleIncomingCall: (data) => {
-            console.log('ye log call lag gaya ', data)
-            set({ isReceiving: true, callData: data });
-        },
-    };
-});
+//         handleIncomingCall: (data) => {
+//             console.log('ye log call lag gaya ', data)
+//             set({ isReceiving: true, callData: data });
+//         },
+//     };
+// });
 
-// Handle Socket Events
-socket.on('call-made', async (data) => {
-    console.log('Incoming call:', data);
-    useSocketStore.getState().handleIncomingCall(data);
-});
-socket.on('call-answered', (data) => {
-    const peerConnection = useSocketStore.getState().peerConnection;
-    peerConnection?.setRemoteDescription(new RTCSessionDescription(data.answer));
-});
-socket.on('receive-candidate', (data) => {
-    useSocketStore.getState().addCandidate(data.candidate);
-});
-socket.on('call-ended', (data) => {
-    useSocketStore.getState().endCall(data);
-});
+// // Handle Socket Events
+// socket.on('call-made', async (data) => {
+//     console.log('Incoming call:', data);
+//     useSocketStore.getState().handleIncomingCall(data);
+// });
+// socket.on('call-answered', (data) => {
+//     const peerConnection = useSocketStore.getState().peerConnection;
+//     peerConnection?.setRemoteDescription(new RTCSessionDescription(data.answer));
+// });
+// socket.on('receive-candidate', (data) => {
+//     useSocketStore.getState().addCandidate(data.candidate);
+// });
+// socket.on('call-ended', (data) => {
+//     useSocketStore.getState().endCall(data);
+// });
